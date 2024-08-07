941c6d64bf4c5fc52f566b363a2e79d5b5f5d523
aa1234aa1234 <162973845+aa1234aa1234@users.noreply.github.com>
2024-08-07 19:56:16 +0900

    Add files via upload

--- Test/src/main/java/com/example/Test/TestController.java
+++ Test/src/main/java/com/example/Test/TestController.java
@@ -1,8 +1,12 @@
 package com.example.Test;
 
+import java.io.BufferedReader;
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.stream.Collectors;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
@@ -21,11 +25,18 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.util.concurrent.SuccessCallback;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
@@ -65,6 +76,56 @@ public class TestController {
 	            .body(resource);
 	}
 	
+	
+	
+	@RequestMapping("/bar")
+    public ResponseEntity<String> invalidcharacterfoundinmethodname() {
+		WebClient client = WebClient.builder()
+				.baseUrl("https://housevalue.co.kr/")
+				.defaultHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36")
+				.build();
+		MultiValueMap<String, String> formdata = new LinkedMultiValueMap<>();
+		formdata.set("address1", "서울");
+		formdata.set("address2", "중구");
+		formdata.set("address3", "명동2가");
+		formdata.set("mountain_yn", "N");
+		formdata.set("main_address_no", "1");
+		formdata.set("sub_address_no", "18");
+		formdata.set("longitude", "126.98586313967616");
+		formdata.set("latitude", "37.56342853082633");
+		formdata.set("d_search", "3bhdpV6xBvdTZeSTB3TH5gcadqImuKS3LDzw1c6OyOHNmUGVNh");
+		formdata.set("only_price", "false");
+		formdata.set("idx", "0");
+		formdata.set("view_this_real", "");
+		formdata.set("view_this_rental", "");
+		formdata.set("view_likeness_real", "");
+		formdata.set("view_likeness_rental", "");
+		formdata.set("view_likeness_real2", "");
+		formdata.set("view_likeness_real3", "");
+		formdata.set("view_contract_real", "");
+		formdata.set("view_close_real", "");
+		formdata.set("view_contract_rental", "");
+		formdata.set("view_close_rental", "");
+		formdata.set("view_contract_real2", "");
+		formdata.set("view_close_real2", "");
+		formdata.set("view_contract_real3", "");
+		formdata.set("view_close_real3", "");
+		WebClient.ResponseSpec response = client.post()
+		.uri("/h_data/jibun_detail/real_price")
+		.bodyValue(formdata)
+		.retrieve();
+		
+		System.out.println(client.toString());
+		JSONObject obj = new JSONObject(response.toEntity(String.class).block().getBody());
+//		for(Map.Entry<String,Object> e : obj.toMap().entrySet()) {
+//			System.out.println(e);
+//		}
+		//System.out.println(obj.get("gunmul_result"));
+		return ResponseEntity.ok()
+				.body(obj.toString());
+    }
+	
+	
 //	@MessageMapping("/chat/image")
 //	@SendTo("/aa")
 //	public Message sendimage(@Payload Message data) {
@@ -84,11 +145,17 @@ public class TestController {
 	}
 	
 	@MessageMapping("/testinghaha")
-	@ResponseBody
-	public ResponseEntity<String> create() {
-	    return ResponseEntity.status(HttpStatus.CREATED)
-	       .contentType(MediaType.TEXT_PLAIN)
-	       .body("Custom string answer");
+	public void create() {
+		WebClient a = WebClient.builder()
+				.baseUrl("http://localhost:8080/")
+				.build();
+		ResponseEntity<String> res = a.post()
+		.uri("/bar")
+		.retrieve().toEntity(String.class).block();
+		System.out.println(res.getBody());
+//	    return ResponseEntity.status(HttpStatus.CREATED)
+//	       .contentType(MediaType.TEXT_PLAIN)
+//	       .body("Custom string answer");
 	}
 	
 	@MessageMapping("/chat")
