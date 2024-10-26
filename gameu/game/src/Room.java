d65bca7b562c4fdf0e39417e292ef9bf5f2b8835
aa1234aa1234 <162973845+aa1234aa1234@users.noreply.github.com>
2024-10-26 13:38:42 +0900

    Add files via upload

---
+++ gameu/game/src/Room.java
@@ -0,0 +1,31 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Room {
+    private BufferedImage image;
+    private Rectangle size;
+    private Point spawnpoint;
+    private ArrayList<Asset> assets = new ArrayList<>();
+
+    public Room(String imagepath, Rectangle size, Point spawnpoint) {
+        try {
+            image = ImageIO.read(new File(imagepath));
+            this.size = size;
+            this.spawnpoint = spawnpoint;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public Point getSpawnPoint() {
+        return spawnpoint;
+    }
+}
