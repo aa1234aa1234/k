cfd4c314f14e7c5f678d94c2f5362f342e6008f7
aa1234aa1234 <162973845+aa1234aa1234@users.noreply.github.com>
2024-10-27 09:49:17 +0900

    Add files via upload

---
+++ gameu/game/src/Asset.java
@@ -0,0 +1,11 @@
+public class Asset {
+    private HitBox collision;
+
+    public HitBox getCollision() {
+        return collision;
+    }
+
+    public void setCollision(HitBox collision) {
+        this.collision = collision;
+    }
+}
