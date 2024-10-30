e949b054ad5109b18354946423bf655c87997363
aa1234aa1234 <162973845+aa1234aa1234@users.noreply.github.com>
2024-10-30 19:53:21 +0900

    Add files via upload

---
+++ game/src/HitBox.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+
+public class HitBox extends Rectangle{
+    public HitBox(int width, int height, Point pos) {
+        setSize(width,height);
+        setLocation(pos);
+    }
+
+}
