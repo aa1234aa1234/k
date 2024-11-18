56f3b7e9a083ea42a13b90429aded24d5406cdad
aa1234aa1234 <162973845+aa1234aa1234@users.noreply.github.com>
2024-11-18 19:51:16 +0900

    Delete game(1) directory

--- game(1)/src/HitBox.java
+++
@@ -1,9 +0,0 @@
-import java.awt.*;
-
-public class HitBox extends Rectangle{
-    public HitBox(int width, int height, Point pos) {
-        setSize(width,height);
-        setLocation(pos);
-    }
-
-}
