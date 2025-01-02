345739c50b581105895597c5149a1d9ae838a5b1
aa1234aa1234 <162973845+aa1234aa1234@users.noreply.github.com>
2025-01-02 19:51:23 +0900

    Update fewa.py

--- fewa.py
+++ fewa.py
@@ -37,18 +37,22 @@ def run(dataset) -> None:
             #input("")
             page.wait_for_load_state('load')
         def bruh():
-            page.wait_for_selector('span[class="point02"]',timeout=5000)
-            print('loaded')
-            lol = page.evaluate("""()=> {
-                var a = document.querySelector('span[class=\"point02\"]');
-                return a ? a.innerText : null;
-            }""")
-            if lol is None:
+            try:
+                page.wait_for_selector('span[class="point02"]',timeout=5000)
+                print('loaded')
+                lol = page.evaluate("""()=> {
+                    var a = document.querySelector('span[class=\"point02\"]');
+                    return a ? a.innerText : null;
+                }""")
+                if lol is None:
+                    return
+                curs.execute(f"DELETE FROM Ticket;")
+                conn.commit()
+                curs.execute(f"INSERT INTO stock.kakaoMsg VALUES (0,'010-3782-2292','{lol}',now());")
+                conn.commit()
+            except Exception as e:
+                print(e,end=" load event")
                 return
-            curs.execute(f"DELETE FROM Ticket;")
-            conn.commit()
-            curs.execute(f"INSERT INTO stock.kakaoMsg VALUES (0,'010-3782-2292','{lol}',now());")
-            conn.commit()
         def domchange():
             page.evaluate(f"""() => {{
                 var a = document.createElement('a').onclick = window['f_close'];
@@ -74,14 +78,12 @@ def run(dataset) -> None:
         while True:
             try:
                 page.wait_for_load_state('load')
-                fwe = page.evaluate("() => !!document.getElementById('btn_next');")
                 
-                print(fwe)
                 
-                woah = page.locator('a').filter(has=page.locator('img[alt="예약하기"]')).nth(0)
-                woah.wait_for()
-                if fwe is True:
-                    raise Exception("lmao")
+                
+                #woah = page.locator('a').filter(has=page.locator('img[alt="예약하기"]')).nth(0)
+                #woah.wait_for()
+                
                 
                 #if woah.count() > 0:
                 #    print("haha")
@@ -97,35 +99,54 @@ def run(dataset) -> None:
                     btn = thing.nth(i).locator('td').nth(5)
                     if btn.evaluate("a => a.innerHTML") == "-":
                         continue
-                    departtime = datetime.strptime(thing.nth(i).locator('td').nth(2).evaluate("a => a.innerHTML").strip().split("<br>")[1],"%H:%M")
-                    time = datetime.strptime(thing.nth(i).locator('td').nth(3).evaluate("a => a.innerHTML").strip().split("<br>")[1],"%H:%M")
-                    if departtime < datetime.now()+timedelta(minutes=20):
+                    departtime = datetime.strptime(thing.nth(i).locator('td').nth(2).evaluate("a => a.innerHTML").strip().split("<br>")[1],"%H:%M").replace(
+                        year=datetime.now().year, month = datetime.now().month, day = datetime.now().day
+                    )
+                    time = datetime.strptime(thing.nth(i).locator('td').nth(3).evaluate("a => a.innerHTML").strip().split("<br>")[1],"%H:%M").replace(
+                        year=datetime.now().year, month = datetime.now().month, day = datetime.now().day
+                    )
+                    time2 = datetime.strptime(date[4],"%H").replace(
+                        year=datetime.now().year, month = datetime.now().month, day = datetime.now().day
+                    )
+                    if departtime > datetime.now()-timedelta(minutes=20) and departtime < datetime.now()+timedelta(minutes=20):
                         continue
                     if time.hour < datetime.strptime(thing.nth(i).locator('td').nth(2).evaluate("a => a.innerHTML").strip().split("<br>")[1],"%H:%M").hour:
                         time += timedelta(days=1)
-                    if time <= datetime.strptime(date[4],"%H"):
+                    if time2.hour < int(date[3]):
+                        time2 += timedelta(days=1)
+                    if time <= time2:
                         flagthing = False
                         btn.locator('a:has(img[alt="예약하기"])').click()
+                        frame = page.locator('iframe[title="열차안내"]')
                         page.wait_for_event('dialog')
+                        print(departtime)
                         break
                 if flagthing is True:
                     few = page.locator('a:has(img[src="/docs/2007/img/common/btn_next.gif"])').click(timeout=10000)
+                    page.wait_for_selector('a:has(img[alt="다음"])',timeout=5000)
+                    fwe = page.query_selector('a:has(img[alt="다음"])')
+                    print(fwe)
+                    if fwe is None:
+                        #raise Exception("lmao")
+                        page.locator('a[href="javascript:inqSchedule()"]').click()
                 else:
+                    raise Exception("fuck it")
                     break
                 #aa = page.evaluate("()=>document.querySelector('img[src=\"/docs/2007/img/common/btn_next.gif\"]');")
             except Exception as e:
-                print(e)
-                #page.locator('a[href="javascript:inqSchedule()"]').click()
+                print(e,end="loop")
                 break
+                
         
         #input("fewa")
         #time.sleep(5)
         
-        page.wait_for_selector('table[class="tbl_h"]')
+        #page.wait_for_selector('table[class="tbl_h"]')
+        page.wait_for_url('https://www.letskorail.com/ebizprd/EbizPrdTicketPr12111_i1.do')
         
         cols = page.locator('table[class="tbl_h"]').nth(1).locator("td")
         wa = "승차일자: i0\n열차번호: i1\n열차종별: i2\n출발역: i3\n출발시각: i4\n도착역: i5\n도착시각: i6\n예약매수: i7\n총결제금액: i8"
-        print(cols.count())
+        print(cols.count(),end=" cols")
         for i in range(cols.count()):
             a = cols.nth(i).inner_text().replace(" ", "")
             print(i)
