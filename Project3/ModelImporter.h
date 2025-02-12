969ffe698f1efc4fa2afe2417d63f730b3577592
aa1234aa1234 <162973845+aa1234aa1234@users.noreply.github.com>
2025-02-12 16:21:20 +0900

    Add files via upload

---
+++ Project3/ModelImporter.h
@@ -0,0 +1,55 @@
+#pragma once
+#include "header.h"
+#include <string>
+#include <stb/stb_image.h>
+
+struct Vertex {
+	glm::vec3 Position;
+	glm::vec3 Normal;
+	glm::vec2 TexCoords;
+};
+
+class ModelImporter {
+public:
+	ModelImporter(const char* filepath) {
+		
+	}
+	unsigned int getTextureFromFile(const char* filepath, std::string& directory, bool gamma = false) {
+		std::string filename = std::string(filepath);
+		filename = directory + '/' + filename;
+		unsigned int id;
+		glGenTextures(1, &id);
+		int width, height, components;
+		unsigned char* data = stbi_load(filename.c_str(), &width, &height, &components, 0);
+		if (data) {
+			GLenum format;
+			switch (components) {
+			case 1:
+				format = GL_RED;
+				break;
+			case 3:
+				format = GL_RGB;
+				break;
+			case 4:
+				format = GL_RGBA;
+				break;
+			}
+			glBindTexture(GL_TEXTURE_2D, id);
+			glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+			glGenerateMipmap(GL_TEXTURE_2D);
+
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+			stbi_image_free(data);
+			//glBindTexture(GL_TEXTURE_2D, 0);
+		}
+		else {
+			std::cout << "texture failed to load " << filepath << std::endl;
+			stbi_image_free(data);
+		}
+
+		return id;
+	}
+};
---
+++ Project3/header.h
@@ -0,0 +1,13 @@
+#pragma once
+#define GLAD_GL_IMPLEMENTATION
+#include <glad/gl.h>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+
+#include <iostream>
+#include <vector>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include "ModelImporter.h"
